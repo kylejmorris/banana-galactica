8eb674f601731a83945ef791b1249e7c5a3f49d2
kyle <kyle@banana.dev>
2022-11-15 20:29:30 +0000

    installing from src as tmp fix bcz pip isn't working

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from transformers import pipeline
 import torch
+import galai as gal
+
+model = gal.load_model("standard")
+model.generate("Scaled dot product attention:\n\n\\[")
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
+   
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = gal.load_model("standard")
+ 
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -20,7 +24,7 @@ def inference(model_inputs:dict) -> dict:
         return {'message': "No prompt provided"}
     
     # Run the model
-    result = model(prompt)
+    result = model.generate(prompt)
 
     # Return the results as a dictionary
     return result
