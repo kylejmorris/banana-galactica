1de1426737d32e6adcac4df9b755deab36c1c51e
kyle <kyle@banana.dev>
2022-11-15 21:13:51 +0000

    following my own instructions on how to use banana lol

--- download.py
+++ download.py
@@ -3,11 +3,11 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import pipeline
+import galai as gal
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    model = gal.load_model("standard")
 
 if __name__ == "__main__":
     download_model()
